0305f1571dfb0b41fd989053c2e5dc01548182ad
sandy1k07 <sandarbh3007@gmail.com>
2025-09-02 22:26:05 +0530

    fixed out of stock in cart

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -43,7 +43,7 @@ const Navbar = () => {
             <div className="hidden sm:flex items-center gap-8">
                 <NavLink to='/'>Home</NavLink>
                 <NavLink to='/allproducts'>Products</NavLink>
-                <NavLink to='/'>Contact Us</NavLink>
+                <NavLink to='/seller'>Seller</NavLink>
 
 
                 <div className="hidden lg:flex items-center text-sm gap-2 border border-gray-300 px-3 rounded-full">
--- server/controllers/cartController.js
+++ server/controllers/cartController.js
@@ -1,3 +1,4 @@
+import Product from "../models/Product.js";
 import User from "../models/User.js";
 
 // Get cart data upon login
@@ -44,4 +45,54 @@ const updateCart = async (req, res) => {
     }
 } 
 
-export {updateCart, getCart}
+const checkCart = async (req, res) => {
+    const {cart} = req.body;
+    
+    const userId = req.userId;
+
+    try {
+        const cartItemIds = Object.keys(cart);
+        const products = await Product.find({ '_id': { $in: cartItemIds } });
+
+        let updatedCart = {};
+        let isUpdated = false;
+
+        const productMap = new Map();
+        products.forEach(item => {
+            productMap.set(item._id.toString(), item);
+        })
+        
+        for (let productId in cart) {
+            const product = productMap.get(productId);
+            if (product.inStock) {
+                updatedCart[productId] = cart[productId];
+            }else{
+                isUpdated = true;
+            }
+        }
+        
+        if(!isUpdated){
+            return res.json({
+                success: true,
+                updated: false,
+            })
+        }
+        const user = await User.findByIdAndUpdate(userId, {cart: updatedCart});
+        
+        return res.json({
+            success: true,
+            updated: true,
+            cart: updatedCart
+        })
+
+    } catch (error) {
+        console.log(error.message);
+        
+        return res.json({
+            success: false,
+            msg: error.message,
+        })
+    }
+}
+
+export {updateCart, getCart, checkCart}
